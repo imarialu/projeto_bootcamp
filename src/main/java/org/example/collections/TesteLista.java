08adb90df867ac0f118bf2bd69ce81219c34dc89
imarialu <marialuizaprado13@gmail.com>
2025-03-21 18:52:44 -0300

    lista

---
+++ src/main/java/org/example/collections/TesteLista.java
@@ -0,0 +1,24 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteLista {
+
+    public static void main(String[] args) {
+
+        List<String> contas = new ArrayList(); // Lista de strings
+
+        contas.add("Conta Poupança");
+        contas.add("Conta Corrente");
+        contas.add("Conta Pagamento");
+        contas.add("Conta Poupança");
+
+
+        // Pega os elementos através de índice
+        System.out.println("Conta 1: " + contas.get(0));
+        System.out.println("Conta 2: " + contas.get(1));
+        System.out.println("Conta 3: " + contas.get(2));
+        System.out.println("Conta 4: " + contas.get(3)); // Valor repetido
+    }
+}
