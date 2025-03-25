ed9900ddc03a8d5107bc6a07e5427f713fdfb617
imarialu <marialuizaprado13@gmail.com>
2025-03-24 21:23:39 -0300

    testando o optional

--- src/main/java/org/example/optional/TesteOptional.java
+++ src/main/java/org/example/optional/TesteOptional.java
@@ -1,8 +1,28 @@
 package org.example.optional;
 
+import org.example.collections.set.Conta;
+import java.util.Optional;
+
 public class TesteOptional {
 
     public static void main(String[] args) {
+
+        Optional<Conta> optionalConta = obtemContaSemErro();
+
+        // Verifica se há uma conta presente no Optional
+        if (optionalConta.isPresent()) {
+            System.out.println("Saldo: " + optionalConta.get().getSaldo());
+        } else {
+            System.out.println("A conta não foi localizada");
+        }
+    }
+
+    public static Conta obtemConta() {
+        return null;
+    }
+
+    public static Optional<Conta> obtemContaSemErro() {
+        return Optional.empty();
 
     }
 }
